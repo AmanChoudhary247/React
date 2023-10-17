d98a7a2c1f3416d72771376ef82cd5a8e4727586
Aman <amanchoudhary09032000@gmail.com>
2023-10-17 15:36:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,38 +4,114 @@ import React from 'react';
 
 
 class InnerComponent extends React.Component
+
 {
   render()
   {
     return <div>Inner component is here </div>
   }
+   
+  componentDidMount()
+  {
+    console.log("####Mounting InnerComponent####")
+  }
+
+  componentWillUnmount()
+  {
+    console.log("****Unmounting InnerComponent####")
+  }
+
 }
+
+
 
 class App extends React.Component
-{
-  state ={counter:0, InnerComponent:-InnerComponent/}
 
-}
+      
 
 {
-  state = {counter:10}
+  state ={counter:0, InnerComponent:<InnerComponent/>}
   constructor(props)
+
+
   {
     super(props)
     console.log("I am inside constructer")
   }
 
+   static getDerivedStateFromProps()
+
+  {
+    console.log("I am inside getDerivedStateFromProps")
+    return true
+  }
+
+  shouldComponentUpdate()
+  {
+    console.log("Inside ShouldComponentUpdate")
+    return true
+  }
+
+
+
   incrementCounter = () =>
   {
     this.setState({counter:this.state.counter+1})
-    this.setState({InnerComponent:<div>Unmounted</div>})
+    if(this.state.counter % 2==0)
+    {
+      this.setState({innerComponent:<InnerComponent/>})
+    }
+    else
+    {
+      this.setState({innerComponent:<div>Unmounted</div>})
+    }
   }
 
 render()
 {
+  console.log("Inside The Render Method")
+  console.log(this.props)
+  return (
+    <div>
+      <button onClick={this.incrementCounter}>Click ME</button>
+      <br/>
+      counter:{this.state.counter}
+      <br/>
+      <div>{this.state.innerComponent}</div>
+    </div>
+  )
+}
+
+getSnapshotBeforeUpdate(prevProps, prevState)
+{
+  console.log("Inside getSnapshotBeforeUpdate")
+    console.log("Previous Prop:")
+    console.log(prevProps)
+    console.log("Previous State:")
+    console.log(prevState)
+    console.log("--------------------")
+    console.log("Updated Prop:")
+    console.log(this.props)
+    console.log("Update State:")
+    console.log(this.state)
+    return true
+}
+componentDidMount()
+  {
+    console.log("I am inside componentDidMount")
+  }
+}
+
+
+
+/*
+
+{
   return <button onClick={this.props.clickEvent}>Click me</button>
 }
-}
+
+
+
 
 /*
 function App(props) {
